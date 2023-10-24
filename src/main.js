297cab8655dc2cadc2f1557a43391f5dadda6d86
liulin <xiaolin201809@126.com>
2023-10-25 01:45:24 +0800

    feat: GUI基本操作

--- src/main.js
+++ src/main.js
@@ -1,19 +1,12 @@
 import * as THREE from 'three'
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+import {OrbitControls} from "three/addons/controls/OrbitControls.js";
 import Stats from "three/addons/libs/stats.module.js";
+import {GUI} from "three/addons/libs/lil-gui.module.min.js";
 // 创建一个场景
 const scene = new THREE.Scene()
 
 // 创建球体
-const geometry = new THREE.SphereGeometry(10, 50, 50)
-// // 创建材质对象：漫反射材质
-// const material = new THREE.MeshLambertMaterial({
-//     color: 0xffdd00,
-//     wireframe: false,
-//     transparent: true,
-//     opacity: 0.5,
-//     side: THREE.DoubleSide, // 正反面可见
-// })
+const geometry = new THREE.BoxGeometry(50, 50, 50)
 
 const material = new THREE.MeshPhongMaterial({
     color: 0x00ffff,
@@ -51,21 +44,71 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(window.innerWidth, window.innerHeight)
 // 设置像素比，以免渲染模糊，是一个优化项
 renderer.setPixelRatio(window.devicePixelRatio)
-// 设置渲染颜色及透明度
-renderer.setClearColor(0x444444, 0.5)
 renderer.render(scene, camera)
 document.body.appendChild(renderer.domElement)
 
-console.log(`查看当前设备像素比：${window.devicePixelRatio}`)
-
 // 创建一个三维坐标系
-const axesHelper = new THREE.AxesHelper(15)
+const axesHelper = new THREE.AxesHelper(100)
 scene.add(axesHelper)
 
 // 添加轨道控制器
 const controls = new OrbitControls(camera, renderer.domElement)
 
 controls.update()
+
+// 创建GUI对象
+const gui = new GUI()
+// 改变 UI 交互界面样式
+gui.domElement.style.right = '10px'
+gui.domElement.style.width = '270px'
+
+gui.title('参数控制')
+
+const obj = {x: 30, bool: true}
+
+gui.add(obj, 'x', 0, 100, 1).name('修改X值').onChange(x => {
+    console.log(`obj.x = ${x}`)
+    mesh.position.x = x
+})
+
+// gui.add(mesh.position, 'x', 0, 100).name('长方体X值').step(1)
+// gui.add(mesh.position, 'y', 0, 100).name('长方体Y值').step(1)
+
+// 布尔值类型
+gui.add(obj, 'bool').name('是否旋转').onChange(val => {
+    console.log(obj, val)
+})
+
+// 下拉选项
+gui.add(mesh.position, 'x', {
+    '左侧': -100,
+    '居中': 0,
+    '右侧': 100
+}).name('长方体X值').step(1)
+gui.add(mesh.position, 'y', [-100, 0, 100]).name('长方体Y值').step(1)
+
+gui.add(mesh.position, 'z', 0, 100).name('长方体Z值').step(1)
+// gui.addColor(mesh.material, 'color').onChange(color => {
+//     console.log(`物品颜色为：`, color)
+// })
+
+// gui.add(ambientLight, 'intensity', 0, 2.0).name('环境光强度').step(0.01).onChange(val => {
+//     console.log(`当前环境光强度为：${val}`)
+// })
+
+// gui.add(directionalLight, 'intensity', 0, 2.0).name('平行光强度').step(0.1)
+
+// GUI 分组
+const materialGui = gui.addFolder().title('物品材质')
+materialGui.addColor(mesh.material, 'color').name('颜色').onChange(color => {
+    console.log(`物品颜色为：`, color)
+})
+
+const lightGui = gui.addFolder().title('光源')
+lightGui.add(ambientLight, 'intensity', 0, 2.0).name('环境光强度').step(0.01).onChange(val => {
+    console.log(`当前环境光强度为：${val}`)
+})
+lightGui.add(directionalLight, 'intensity', 0, 2.0).name('平行光强度').step(0.1)
 
 // 创建性能监控器
 const stats = new Stats()
@@ -76,7 +119,9 @@ document.body.appendChild(stats.domElement)
 const animate = () => {
     // 加载性能监控器
     stats.update()
-    mesh.rotateZ(0.01)
+    if (obj.bool) {
+        mesh.rotateZ(0.01)
+    }
     renderer.render(scene, camera)
     requestAnimationFrame(animate)
 }
